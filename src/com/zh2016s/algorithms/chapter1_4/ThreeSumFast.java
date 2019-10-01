c86df67fd6139bde3a254d90a032f2613b9a646f
zh2016s <zh2016s@163.com>
2019-10-01 08:52:22 +0800

    完成至3-sum问题的N^2lgN解法

---
+++ src/com/zh2016s/algorithms/chapter1_4/ThreeSumFast.java
@@ -0,0 +1,39 @@
+package com.zh2016s.algorithms.chapter1_4;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.zh2016s.algorithms.chapter1_1.BinarySearch;
+
+
+public class ThreeSumFast {
+	public static int count(int[] a) {
+		//计算和为0的三元组的数目
+		Arrays.sort(a);
+		int N = a.length;
+		int cnt = 0;
+		for(int i =0;i<N;i++)
+			for(int j =i;j<N;j++)
+				if(BinarySearch.binarySearch(-a[i]-a[j], a)>j)
+					cnt++;
+		return cnt;
+	}
+	public static void main(String[] args) {
+		try {
+			FileInputStream is = new FileInputStream("algs4-data/1kints.txt");
+			Scanner sc = new Scanner(is);
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			while(sc.hasNextInt())
+				list.add(sc.nextInt());
+			int[] nums = list.stream().mapToInt(Integer::valueOf).toArray();
+			System.out.println(count(nums));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ src/com/zh2016s/algorithms/chapter1_4/TwoSumFast.java
@@ -0,0 +1,44 @@
+package com.zh2016s.algorithms.chapter1_4;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.zh2016s.algorithms.chapter1_1.BinarySearch;
+
+/**
+ * 
+ * @author aaoomm
+ * 2-sum问题的线性对数级别的解法
+ *
+ */
+public class TwoSumFast {
+	public static int count(int[] a) {
+		//计算和为0的整数对的数目
+		Arrays.sort(a);
+		int N = a.length;
+		int cnt = 0;
+		for(int i=0;i<N;i++)
+			if(BinarySearch.binarySearch(-a[i], a)>i)
+				cnt++;
+		return cnt;
+	}
+	public static void main(String[] args) {
+		FileInputStream is;
+		try {
+			is = new FileInputStream("algs4-data/1kints.txt");
+			Scanner sc = new Scanner(is);
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			while(sc.hasNextInt())
+				list.add(sc.nextInt());
+			int[] nums = list.stream().mapToInt(Integer::valueOf).toArray();
+			System.out.println(count(nums));
+			is.close();
+			sc.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
